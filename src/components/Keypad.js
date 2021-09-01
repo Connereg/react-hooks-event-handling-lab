bd011a900ef0997a0d3e933ce2c4f3a8bab018fb
Conner Gleason <connereg@optonline.net>
2021-09-01 09:21:47 -0400

    Completed Lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,30 @@
 // Code Keypad Component Here
+function handleChange(event){
+    event.preventDefault()
+    console.log('Entering password...')
+}
+function handleSubmit(event){
+    event.preventDefault()
+    const thePassEntered = document.querySelector(".passwordField").value
+    if (thePassEntered !== "12345badpassword"){
+        console.log("WRONG PASSWORD! ABORT!")
+
+    } else {
+        console.log("How did you know???")
+    }
+
+}
+
+function Keypad(){
+   return ( 
+   <div>
+        <form onSubmit={handleSubmit}>
+            <input className="passwordField" type="password" placeholder="PASSWORD FIELD" onChange={handleChange} />
+            <button type="submit">Submit Password</button>
+        </form>
+    </div>
+   )
+
+}
+
+export default Keypad;
